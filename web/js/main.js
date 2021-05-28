1210e00ea40ce310f5939aca9016b1213b90c7c8
CodinPedia <82684819+codinPedia@users.noreply.github.com>
2021-05-28 23:17:48 +0100

    Update main.js

--- web/js/main.js
+++ web/js/main.js
@@ -5,9 +5,9 @@ for(var i = 0; i < 4; i++){
   sections = sections+"<section>";
   for(var k = 1; k <= 9; k++){
     if(i%2==0 && k==3){
-      sections = sections+`<video src="https://cdn.videvo.net/videvo_files/video/free/2020-05/small_watermarked/3d_ocean_1590675653_preview.webm" autoplay loop></video>`;
+      sections = sections+`<video src="https://cdn.videvo.net/videvo_files/video/free/2020-05/small_watermarked/3d_ocean_1590675653_preview.webm" playsinline autoplay muted loop></video>`;
     }else if(i%2!=0 && k==1){
-      sections = sections+`<video src="https://cdn.videvo.net/videvo_files/video/free/2020-09/small_watermarked/200710_05_expressions%20portraits_HD_029_preview.webm" autoplay loop></video>`;
+      sections = sections+`<video src="https://cdn.videvo.net/videvo_files/video/free/2020-09/small_watermarked/200710_05_expressions%20portraits_HD_029_preview.webm" playsinline autoplay muted loop></video>`;
     }else{
       sections = sections+`<img src="https://source.unsplash.com/random/400x400?sig=${k}" alt="">`;
     }
